49f9a7caa9d5b8dab40b69aff8c798eaaefa1de9
obolsuro <obolsuro@gmail.com>
2020-12-17 10:47:39 +0700

    baseURL API
    basenya postman

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,5 +1,30 @@
+import API from './index';
 import axios from 'axios';
 
+
+// export const getData = async (country) => {
+//     let prefix = '/all';
+//     if (country) {
+//         prefix = '/countries/' + country;
+//     }
+//     try {
+//         const response = await API.get(prefix);
+//         return response.data;
+//     }
+//     catch (error) {
+//         return error;
+//     }
+// }
+
+// export const getCountriesData = async () => {
+//     try {
+//         const response = await API.get('/countries');
+//         return response.data;
+//     }
+//     catch (error) {
+//         return error;
+//     }
+// }
 
 export const getProvinceData = async () => {
     try {
