9bceaa10c0d91e31d28ee20f0fd0bd6cec3057f7
Ttanus <kelvinadrian777@gmail.com>
2020-12-28 22:48:53 +0700

    test api province

--- src/pages/second/Province.js
+++ src/pages/second/Province.js
@@ -1,8 +1,19 @@
 import "./Province.css";
 import Header from "../../components/layout/header/pages/Pages";
 import Footer from "../../components/layout/footer/Footer";
+import { getProvinceData, getProvinceHospital } from "../../api/Api";
+import React, { useState, useEffect } from "react";
 
 function Province() {
+  const fetchData = async () => {
+    const fetchedData = await getProvinceData();
+    console.log(fetchedData);
+    const fetchedHospital = await getProvinceHospital();
+    console.log(fetchedHospital);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <>
       <Header />
