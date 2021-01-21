c8dc54f5d2d007f4b72159b7362b728aea12a90f
obolsuro <obolsuro@gmail.com>
2021-01-21 18:32:10 +0700

    dataP
    masih Error pake Array juga

--- src/components/tables/DataP.js
+++ src/components/tables/DataP.js
@@ -12,15 +12,22 @@ function Data({ }) {
         const fetchedDataProvince = await getProvinces();
         var i = 1;
         var temp = fetchedDataProvince[1];
-        console.log(temp);
         for (let prov in fetchedDataProvince) {
             temp = fetchedDataProvince[prov];
+            var datasource = [];
+            datasource.push({
+                Provinsi: temp.Provinsi,
+                Positif: temp.Kasus_Posi,
+                Sembuh: temp.Kasus_Semb,
+                Meninggal: temp.Kasus_Meni
+            });
+            console.log(datasource[0]["Provinsi"]);
+
             return (
                 <tr >
-                    <td>{i++}</td>
-                    <td>{temp.Provinsi}</td>
+                    <td>{datasource[0]['Provinsi']}</td>
                     <td>
-                        {((temp.Kasus_Meni + temp.Kasus_Posi + temp.Kasus_Sem).toString().replace(/\B(?=(\d{3})+(?!\d))/g), ".")}
+                        {((datasource[0]['Positif'] + datasource[0]['Sembuh'] + datasource[0]['Meninggal']).toString().replace(/\B(?=(\d{3})+(?!\d))/g), ".")}
                     </td>
                 </tr>);
         }
